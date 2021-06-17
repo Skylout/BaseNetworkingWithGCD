61ba927d488091cc9567a223bbb311f3f8540820
Leonid Shapetko <Magicka@yandex.ru>
2021-06-17 19:59:28 +0300

    Добавлена кнопка, которая меняет собственный цвет. Просто забавы ради

--- BaseNetworkingWithGCD/View Controllers/mainVC.swift
+++ BaseNetworkingWithGCD/View Controllers/mainVC.swift
@@ -10,6 +10,7 @@ import UIKit
 class mainVC: UIViewController {
 
     @IBOutlet weak var imageViewForAnimal: UIImageView!
+    @IBOutlet weak var doNothingButton: UIButton!
     let networkObj = Network()
     
     override func viewDidLoad() {
@@ -26,6 +27,15 @@ class mainVC: UIViewController {
     @IBAction func kittyDownloadAfterFiveSec(_ sender: UIButton) {
         networkObj.getImageByURLSession { (image) in
             self.imageViewForAnimal.image = image
+        }
+    }
+    @IBAction func changeColorOfDoNothingButton(_ sender: UIButton) {
+        DispatchQueue.main.async {
+            if (self.doNothingButton.backgroundColor == .systemBlue) {
+                self.doNothingButton.backgroundColor = .orange
+            } else {
+                self.doNothingButton.backgroundColor = .purple
+            }
         }
     }
 }
